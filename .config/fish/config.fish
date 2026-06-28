a6334fdd9b6c6b55427e3b317f10210ca96dce93
Michael McDonald <835798+codegoyf@users.noreply.github.com>
2026-06-27 21:25:22 -0500

    Initial dotfiles

---
+++ .config/fish/config.fish
@@ -0,0 +1,5 @@
+### MANAGED BY RANCHER DESKTOP START (DO NOT EDIT)
+set --export --prepend PATH "/Users/michael/.rd/bin"
+### MANAGED BY RANCHER DESKTOP END (DO NOT EDIT)
+
+starship init fish | source
